8ec81e74d74cf35f0b1feec2b7fa6d7401314c42
Yashdo <109139471+Yashdo@users.noreply.github.com>
2022-11-23 17:37:01 +0530

    Delete float_number_from_user_side.py

--- float_number_from_user_side.py
+++
@@ -1,8 +0,0 @@
-a=float(input("Enter only float number 1"))
-b=float(input("Enter only float number 1"))
-c=float(input("Enter only float number 1"))
-d=float(input("Enter only float number 1"))
-e=float(input("Enter only float number 1"))
-
-a=[a,b,c,d,e]
-print(a)
