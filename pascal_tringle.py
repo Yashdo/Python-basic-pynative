c267a4d0b9d326358ad86b269813737d4e1c4d7d
Yashdo <yashdobariya20@gmail.com>
2022-12-10 18:25:27 +0530

    Leetcode Exersies

--- pascal_tringle.py
+++ pascal_tringle.py
@@ -1,12 +1,12 @@
-user_input=int(input("Enter your number : "))
+user_input=5
 
 for i in range(1,user_input+1):
 
     for j  in range(i-1,user_input-1):
         print(" ",end=' ')
-
+    m=1
     for j in range(i,i*2):
-        pascal=(i+j-1)*(j-i*i)
-        print(pascal,end=' ')
-
+        print(m, end = " ")
+        m = int(m * (i - j) / j) 
+     
     print("\n")
---
+++ practical_61.py
@@ -0,0 +1,7 @@
+head = [1,2,3,4,5]
+string=str(head)
+k = 2
+for i in string:
+    rev=reversed(i)
+    print(rev)
+    
