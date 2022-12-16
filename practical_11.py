b0012c1d3764957f006eb1294cfe67c2491a4625
Yashdo <yashdobariya20@gmail.com>
2022-12-16 18:27:59 +0530

    Leetcode Exersies

---
+++ practical_11.py
@@ -0,0 +1,14 @@
+# Container With Most Water
+
+height = [1,8,6,2,5,4,8,3,7,8]
+l=0
+r = len(height)-1
+arealist = []
+while l != r :
+    area = (r-l)*min(height[l] , height[r])
+    arealist.append(area)
+    if height[l] < height [r]:
+        l += 1
+    else:
+        r += -1
+print(max(arealist))
